a57e7f0575a792a985973356c830dd1b2c270b77
Kurov_DA <kurovda@gmail.com>
2017-08-31 23:13:32 +0300

    structure, routes, pages, shared resource

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,25 @@
 class PagesController < ApplicationController
   def welcome
   end
+
+  def about
+  end
+
+  def services
+  end
+
+  def careers
+  end
+
+  def customers
+  end
+
+  def projects
+  end
+
+  def partners
+  end
+
+  def contacts
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root  'pages#welcome'
-  get "home" => "pages#welcome"
-  get "/" => "pages#welcome"
+  get 'home'       => 'pages#welcome'
+  get '/'          => 'pages#welcome'
+  get '/about'     => 'pages#about'
+  get '/services'  => 'pages#services'
+  get '/customers' => 'pages#customers'
+  get '/partners'  => 'pages#partners'
+  get '/projects'  => 'pages#projects'
+  get '/careers'   => 'pages#careers'
+  get '/contacts'  => 'pages#contacts'
 
 end
