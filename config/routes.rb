d2f45a7688e2082c9227b2475a3337fb60a29d48
Kurov_DA <kurovda@gmail.com>
2017-08-31 23:18:06 +0300

    bag in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root  'pages#welcome'
-  get 'home'       => 'pages#welcome'
+  get '/home'      => 'pages#welcome'
   get '/'          => 'pages#welcome'
   get '/about'     => 'pages#about'
   get '/services'  => 'pages#services'
