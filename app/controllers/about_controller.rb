3ec2d4a09f8b1834ec41a5b82aaa94b86f896503
Kurov_DA <kurovda@gmail.com>
2017-09-08 20:07:09 +0300

    auth by gem

---
+++ app/controllers/about_controller.rb
@@ -0,0 +1,26 @@
+class AboutController < ApplicationController
+  def welcome
+  end
+
+  def about
+  end
+
+  def services
+  end
+
+  def careers
+  end
+
+  def customers
+  end
+
+  def projects
+  end
+
+  def partners
+  end
+
+  def contacts
+  end
+
+end
